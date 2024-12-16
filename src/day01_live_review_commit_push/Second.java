d3879af0cf36c1a60a528d98eef9adc24b7be364
cvetkovicmilan <cvetkovicmilan88@gmail.com>
2024-12-15 22:14:08 -0500

    Updated Second.java

--- src/day01_live_review_commit_push/Second.java
+++ src/day01_live_review_commit_push/Second.java
@@ -3,6 +3,6 @@ package day01_live_review_commit_push;
 public class Second {
     public static void main(String[] args) {
         System.out.println("Hello from the second class");
-        System.out.println("More changes to second class");
+        System.out.println("More changes to the second class");
     }
 }
